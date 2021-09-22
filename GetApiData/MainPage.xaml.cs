5ed3d72f91c316bde71f614530c704a528336d0b
Ahmedbinfaisal <67901435+AhmedFaisal11@users.noreply.github.com>
2021-09-22 17:53:20 +0500

    Add files via upload

---
+++ GetApiData/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace GetApiData
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var httpclient = new HttpClient();
+            var result = await httpclient.GetStringAsync("https://jsonplaceholder.typicode.com/users");
+            var resultUser = JsonConvert.DeserializeObject<Users[]>(result);
+            User.ItemsSource = resultUser;
+        }
+    }
+}
